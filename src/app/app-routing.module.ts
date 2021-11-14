1e764a3ad3afe3b3a9ed1769845ed93df1cfd557
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2021-11-13 22:49:43 -0330

    update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from "./app.component";
+import {livestreamPageComponent} from "./pages/livestream/livestream.page";
 
 const routes: Routes = [
+  {path: 'livestream', component: livestreamPageComponent}
 ];
 
 @NgModule({
