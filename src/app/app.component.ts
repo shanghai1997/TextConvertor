ef2964c1a64d4d5ea598836b15c465a3e6d62f77
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2021-11-19 15:45:16 -0330

    add click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TextConvertor';
+
+  constructor(private router: Router){
+  }
+
+  switchPage():void {
+    this.router.navigate(['/livestream']);
+  }
 }
