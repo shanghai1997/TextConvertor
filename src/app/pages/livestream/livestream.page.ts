0ff3ba69805b543aa40300e965f78907a442ddd0
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2021-11-29 11:28:31 -0330

    add css

--- src/app/pages/livestream/livestream.page.ts
+++ src/app/pages/livestream/livestream.page.ts
@@ -3,6 +3,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 @Component({
   selector: 'app-livestream',
   templateUrl: './livestream.page.html',
+  styleUrls: ['./livestream.page.css']
 })
 export class LivestreamPageComponent {
   title = 'TextConvertor';
