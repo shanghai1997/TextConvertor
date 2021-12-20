55f4b929e318c556c0f2e297ac0cddb295a8cc57
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2021-12-20 00:01:01 -0330

    add subject and subscribe

--- src/app/pages/livestream/livestream.page.ts
+++ src/app/pages/livestream/livestream.page.ts
@@ -1,18 +1,25 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Subject} from "rxjs";
+import {Subject, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-livestream',
   templateUrl: './livestream.page.html',
   styleUrls: ['./livestream.page.css']
 })
-export class LivestreamPageComponent{
+export class LivestreamPageComponent implements OnInit{
   title = 'TextConvertor';
   @ViewChild('textArea', { static: true }) iframe: ElementRef;
   textSubject: Subject<String> = new Subject<String>();
   array: string[] = [];
 
   constructor() {
+  }
+
+  ngOnInit() {
+    let sub: Subscription = this.textSubject
+      .subscribe((i) => {
+      console.log(i);
+    })
   }
 
   addToIframe(text: string){
