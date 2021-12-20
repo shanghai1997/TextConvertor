b2309ca0bf16eb63a84b31afaa025c4e36b161d7
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2021-12-19 23:55:55 -0330

    add subject

--- src/app/pages/livestream/livestream.page.ts
+++ src/app/pages/livestream/livestream.page.ts
@@ -1,18 +1,23 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-livestream',
   templateUrl: './livestream.page.html',
   styleUrls: ['./livestream.page.css']
 })
-export class LivestreamPageComponent {
+export class LivestreamPageComponent{
   title = 'TextConvertor';
   @ViewChild('textArea', { static: true }) iframe: ElementRef;
+  textSubject: Subject<String> = new Subject<String>();
+  array: string[] = [];
 
   constructor() {
   }
 
   addToIframe(text: string){
     this.iframe.nativeElement.contentWindow.frames.document.body.insertAdjacentHTML('beforebegin', text+'\n');
+    this.textSubject.next(text);
+    this.array.push(text);
   }
 }
