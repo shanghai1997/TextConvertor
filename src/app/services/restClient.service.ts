dbcca1d0dc2ab541341aebbf8d98697c2311f7d6
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2022-01-16 19:16:04 -0330

    api service

---
+++ src/app/services/restClient.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RestClientService {
+  constructor(http: HttpClient) {
+  }
+
+  getToken() {
+    const url_token = 'https://aip.baidubce.com/oauth/2.0/token'
+    const API_KEY = '2YQSSYa41mSYbKl5eSQWuNU9';
+    const SECRET_KEY = 'ZwCarB7ERSehFP7Kr6CM3erYD54NX3gR'
+    const params = {'grant_type': 'client_credentials',
+      'client_id': API_KEY,
+      'client_secret': SECRET_KEY};
+    this.http.post(url_token, params).toPromise().then((response) => {console.log(response)})
+  }
+}
