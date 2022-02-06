dc29ae612c9db273a4044d45f58cf4fcc0920fc9
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2022-02-06 16:28:14 -0330

    clean firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
 import { LivestreamPageComponent } from "./pages/livestream/livestream.page";
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideMessaging,getMessaging } from '@angular/fire/messaging';
-import { providePerformance,getPerformance } from '@angular/fire/performance';
-import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
-import { provideStorage,getStorage } from '@angular/fire/storage';
 import {RestClientService} from "./services/restClient.service";
 
 @NgModule({
@@ -26,19 +15,9 @@ import {RestClientService} from "./services/restClient.service";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAnalytics(() => getAnalytics()),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
-    provideFunctions(() => getFunctions()),
-    provideMessaging(() => getMessaging()),
-    providePerformance(() => getPerformance()),
-    provideRemoteConfig(() => getRemoteConfig()),
-    provideStorage(() => getStorage()),
   ],
   providers: [
-    ScreenTrackingService,UserTrackingService,RestClientService
+    RestClientService
   ],
   bootstrap: [AppComponent]
 })
